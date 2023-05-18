edfe5b8b5241da6b0cf4f3ae5919eb6aa3f96690
polina-kostyukovich <99528525+polina-kostyukovich@users.noreply.github.com>
2023-05-18 11:52:45 +0300

    Refactor beans (#8) (#9)

--- src/main/java/by/fpmibsu/EasyLearning/bean/CardBean.java
+++ src/main/java/by/fpmibsu/EasyLearning/bean/CardBean.java
@@ -2,12 +2,17 @@ package by.fpmibsu.EasyLearning.bean;
 
 public class CardBean extends Bean {
     public CardBean() {
+        moduleId = 0L;
         word = "";
         translation = "";
     }
 
-    public CardBean(Long id, String word, String translation) {
+    public CardBean(Long id, Long moduleId, String word, String translation) {
         super(id);
+        if (moduleId == null) {
+            throw new IllegalArgumentException("moduleId is null");
+        }
+
         if (word == null) {
             throw new IllegalArgumentException("word is null");
         }
@@ -16,6 +21,7 @@ public class CardBean extends Bean {
             throw new IllegalArgumentException("translation is null");
         }
 
+        this.moduleId = moduleId;
         this.word = word;
         this.translation = translation;
     }
@@ -26,8 +32,13 @@ public class CardBean extends Bean {
         }
 
         id = cardBean.getId();
+        moduleId = cardBean.getModuleId();
         word = cardBean.getWord();
         translation = cardBean.getTranslation();
+    }
+
+    public Long getModuleId() {
+        return moduleId;
     }
 
     public String getWord() {
@@ -36,6 +47,13 @@ public class CardBean extends Bean {
 
     public String getTranslation() {
         return translation;
+    }
+
+    public void setModuleId(Long moduleId) {
+        if (moduleId == null) {
+            throw new IllegalArgumentException("moduleId is null");
+        }
+        this.moduleId = moduleId;
     }
 
     public void setWord(String word) {
@@ -66,18 +84,21 @@ public class CardBean extends Bean {
 
         CardBean otherCard = (CardBean) other;
 
-        return id.equals(otherCard.getId()) && word.equals(otherCard.word) && translation.equals(otherCard.translation);
+        return id.equals(otherCard.id) && moduleId.equals(otherCard.moduleId) &&
+                word.equals(otherCard.word) && translation.equals(otherCard.translation);
     }
 
     @Override
     public String toString() {
         return "CardBean{" +
                 "id=" + id +
+                ", moduleId='" + moduleId + '\'' +
                 ", word='" + word + '\'' +
                 ", translation='" + translation + '\'' +
                 '}';
     }
 
+    Long moduleId;
     private String word;
     private String translation;
 }
