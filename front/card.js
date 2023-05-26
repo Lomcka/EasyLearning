a816ba3a757a6e2a740da7e45f880cd34bf86aec
Iren137 <irishavoronkina@gmail.com>
2023-05-26 20:30:27 +0300

    fix cards' view bug

--- front/card.js
+++ front/card.js
@@ -8,11 +8,11 @@ const repeatPower = data.repeat.length;
 const totalPower = okPower + repeatPower;
 
 const chartData = [
-{
-values: [okPower, totalPower - okPower],
-labels: ['OK', 'Repeat'],
-type: 'pie'
-}
+    {
+        values: [okPower, totalPower - okPower],
+        labels: ['OK', 'Repeat'],
+        type: 'pie'
+    }
 ];
 
 Plotly.newPlot('chart', chartData);
@@ -21,31 +21,33 @@ const continueBtn = document.getElementById('continue-btn');
 const resetBtn = document.getElementById('reset-btn');
 
 continueBtn.addEventListener('click', () => {
-fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
-            'queryType': 'return-cards-to-repeat'
-        }), {
+    const data = {}; // здесь должен быть ваш объект данных
+
+    fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
+        'queryType': 'return-cards-to-repeat'
+    }), {
         method: 'POST',
-        }
-body: JSON.stringify(data)
-})
-.then(() => {
-window.location.href = '/card.html?data=' + JSON.stringify(data);
+        body: JSON.stringify(data)
+    })
+        .then(() => {
+            window.location.href = '/card.html?data=' + JSON.stringify(data);
+        });
 });
-});
+
 
 resetBtn.addEventListener('click', () => {
-const newData = {
-ok: [...data.ok, ...data.repeat],
-repeat: []
-};
+    const newData = {
+        ok: [...data.ok, ...data.repeat],
+        repeat: []
+    };
 
-fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
-            'queryType': 'return-cards-to-repeat'
-        }), {
-method: 'POST',
-body: JSON.stringify(newData)
-})
-.then(() => {
-window.location.href = '/card.html?data=' + JSON.stringify(newData);
-});
+    fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
+        'queryType': 'return-cards-to-repeat'
+    }), {
+        method: 'POST',
+        body: JSON.stringify(newData)
+    })
+        .then(() => {
+            window.location.href = '/card.html?data=' + JSON.stringify(newData);
+        });
 });
--- front/cards.js
+++ front/cards.js
@@ -7,77 +7,77 @@ let originalOrder = [];
 
 // Функция для загрузки карточек из JSON-файла
 function loadCards() {
-  fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
-              'queryType': 'get-cards-to-repeat'
-          }), {
-    .then(response => response.json())
-    .then(data => {
-      // Сохраняем исходный порядок карточек в переменную originalOrder
-      originalOrder = data.map((_, index) => index);
-      renderCards(data, originalOrder);
+    fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
+        'queryType': 'get-cards-to-repeat'
     })
-    .catch(error => console.error(error));
+        .then(response => response.json())
+        .then(data => {
+            // Сохраняем исходный порядок карточек в переменную originalOrder
+            originalOrder = data.map((_, index) => index);
+            renderCards(data, originalOrder);
+        })
+        .catch(error => console.error(error)));
 }
 
 // Функция для отображения карточек на странице
 function renderCards(cards, order) {
-  cardContainer.innerHTML = '';
-  order.forEach(i => {
-    const card = document.createElement('div');
-    const front = document.createElement('div');
-    const back = document.createElement('div');
+    cardContainer.innerHTML = '';
+    order.forEach(i => {
+        const card = document.createElement('div');
+        const front = document.createElement('div');
+        const back = document.createElement('div');
 
-    card.classList.add('card');
-    front.classList.add('front');
-    back.classList.add('back');
-    if (cards[i].status === 'ok') {
-      card.classList.add('ok');
-    } else if (cards[i].status === 'repeat') {
-      card.classList.add('repeat');
-    }
+        card.classList.add('card');
+        front.classList.add('front');
+        back.classList.add('back');
+        if (cards[i].status === 'ok') {
+            card.classList.add('ok');
+        } else if (cards[i].status === 'repeat') {
+            card.classList.add('repeat');
+        }
 
-    front.textContent = cards[i].front;
-    back.textContent = cards[i].back;
+        front.textContent = cards[i].front;
+        back.textContent = cards[i].back;
 
-    card.appendChild(front);
-    card.appendChild(back);
+        card.appendChild(front);
+        card.appendChild(back);
 
-    // Добавляем обработчик клика на карточку
-    card.addEventListener('click', () => {
-      back.classList.toggle('show');
+        // Добавляем обработчик клика на карточку
+        card.addEventListener('click', () => {
+            back.classList.toggle('show');
+        });
+
+        cardContainer.appendChild(card);
     });
-
-    cardContainer.appendChild(card);
-  });
 }
 
 // Обработчик клика на кнопку "Menu"
 menuButton.addEventListener('click', () => {
-  menu.classList.toggle('show');
+    menu.classList.toggle('show');
 });
 
 // Обработчик клика на кнопку "More"
 moreButton.addEventListener('click', () => {
-  menu.classList.toggle('show');
+    menu.classList.toggle('show');
 });
 
 // Обработчик изменения значения радио-кнопок в выпадающем меню
 document.querySelectorAll('input[name="sort-by"]').forEach(radio => {
-  radio.addEventListener('change', event => {
-    switch (event.target.value) {
-      case 'ok':
-        renderCards(cardsData, sortCardsByStatus('ok'));
-        break;
-      case 'repeat':
-        renderCards(cardsData, sortCardsByStatus('repeat'));
-        break;
-      case 'original-order':
-        renderCards(cardsData, originalOrder);
-        break;
-      default:
-        console.error(`Unknown sort option: ${event.target.value}`);
-    }
-  });
+    radio.addEventListener('change', event => {
+        switch (event.target.value) {
+            case 'ok':
+                renderCards(cardsData, sortCardsByStatus('ok'));
+                break;
+            case 'repeat':
+                renderCards(cardsData, sortCardsByStatus('repeat'));
+                break;
+            case 'original-order':
+                renderCards(cardsData, originalOrder);
+                break;
+            default:
+                console.error(`Unknown sort option: ${event.target.value}`);
+        }
+    });
 });
 
 // Загружаем карточки при запуске страницы
