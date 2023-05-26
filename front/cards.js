b3e6d0c8af0c7787eec9198e4d684c25f0f94ec6
Iren137 <irishavoronkina@gmail.com>
2023-05-26 23:13:12 +0300

    fix cards learning

--- front/cards.js
+++ front/cards.js
@@ -1,9 +1,121 @@
-const menuButton = document.querySelector('.menu-button');
-const moreButton = document.querySelector('.more-button');
-const menu = document.querySelector('.menu');
 const cardContainer = document.querySelector('.card-container');
+const modal = document.querySelector('.modal');
+const modalTitle = document.querySelector('.modal-header .title');
+const okButton = document.querySelector('.modal-footer .ok-button');
+const repeatButton = document.querySelector('.modal-footer .repeat-button');
+const closeButton = document.querySelector('.modal-header .close-button');
+let currentIndex = 0;
+let cards = [];
+let randomOrder = true;
+let showWordFirst = true;
+let okCards = [];
+let repeatCards = [];
 
-let originalOrder = [];
+function showModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'block';
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'none';
+}
+function showCard(index) {
+    const card = cards[index];
+    const currentCard = document.createElement('div');
+    currentCard.classList.add('card');
+    currentCard.innerHTML = `
+    <div class="word">${showWordFirst ? card.word : card.translation}</div>
+    <div class="translation">${showWordFirst ? card.translation : card.word}</div>
+  `;
+    cardContainer.innerHTML = '';
+    cardContainer.appendChild(currentCard);
+}
+
+function toggleTranslation() {
+    const currentCard = cardContainer.querySelector('.card');
+    currentCard.classList.toggle('show-translation');
+}
+
+function toggleRandomOrder() {
+    randomOrder = !randomOrder;
+}
+
+function toggleShowWordFirst() {
+    showWordFirst = !showWordFirst;
+}
+
+function handleRightAnswer() {
+    const currentCard = cardContainer.querySelector('.card');
+    currentCard.classList.add('right-answer');
+    setTimeout(() => {
+        currentIndex++;
+        if (currentIndex >= cards.length) {
+            window.location.href = `card.html?ok=${JSON.stringify(okCards)}&repeat=${JSON.stringify(repeatCards)}`;
+        } else {
+            currentCard.classList.remove('right-answer');
+            showCard(currentIndex);
+        }
+    }, 500);
+    okCards.push(cards[currentIndex]);
+}
+
+function handleWrongAnswer() {
+    const currentCard = cardContainer.querySelector('.card');
+    currentCard.classList.add('wrong-answer');
+    setTimeout(() => {
+        currentIndex++;
+        if (currentIndex >= cards.length) {
+            window.location.href = `card.html?ok=${JSON.stringify(okCards)}&repeat=${JSON.stringify(repeatCards)}`;
+        } else {
+            currentCard.classList.remove('wrong-answer');
+            showCard(currentIndex);
+        }
+    }, 500);
+    repeatCards.push(cards[currentIndex]);
+}
+
+function openModal() {
+    modal.style.display = 'flex';
+}
+
+function initModal() {
+    modalTitle.textContent = 'Settings';
+    repeatButton.textContent = 'Заново';
+    closeButton.innerHTML = '&times;';
+    closeButton.addEventListener('click', closeModal);
+    okButton.addEventListener('click', () => {
+        toggleRandomOrder();
+        closeModal();
+    });
+    repeatButton.addEventListener('click', () => {
+        toggleRandomOrder();
+        closeModal();
+    });
+}
+
+document.addEventListener('keydown', event => {
+    if (event.code === 'Space') {
+        toggleTranslation();
+    } else if (event.code === 'ArrowRight') {
+        handleRightAnswer();
+    } else if (event.code === 'ArrowLeft') {
+        handleWrongAnswer();
+    }
+});
+
+function toggleOrder() {
+    var orderText = document.getElementById("order-text");
+    if (orderText.innerHTML === "Прямой порядок") {
+        orderText.innerHTML = "Перемешанные";
+        // Добавляем функционал для опции "Перемешанные"
+        toggleRandomOrder();
+    } else {
+        orderText.innerHTML = "Прямой порядок";
+        // Добавляем функционал для опции "Прямой порядок"
+        toggleShowWordFirst();
+    }
+}
 
 // Функция для загрузки карточек из JSON-файла
 function loadCards() {
@@ -51,34 +163,6 @@ function renderCards(cards, order) {
     });
 }
 
-// Обработчик клика на кнопку "Menu"
-menuButton.addEventListener('click', () => {
-    menu.classList.toggle('show');
-});
 
-// Обработчик клика на кнопку "More"
-moreButton.addEventListener('click', () => {
-    menu.classList.toggle('show');
-});
-
-// Обработчик изменения значения радио-кнопок в выпадающем меню
-document.querySelectorAll('input[name="sort-by"]').forEach(radio => {
-    radio.addEventListener('change', event => {
-        switch (event.target.value) {
-            case 'ok':
-                renderCards(cardsData, sortCardsByStatus('ok'));
-                break;
-            case 'repeat':
-                renderCards(cardsData, sortCardsByStatus('repeat'));
-                break;
-            case 'original-order':
-                renderCards(cardsData, originalOrder);
-                break;
-            default:
-                console.error(`Unknown sort option: ${event.target.value}`);
-        }
-    });
-});
-
-// Загружаем карточки при запуске страницы
 loadCards();
+initModal();
