09732ee5866b63fabddd2fad68834672b2c69e26
Iren137 <irishavoronkina@gmail.com>
2023-05-27 18:53:43 +0300

    relocate files

---
+++ front/java script/modules.js
@@ -0,0 +1,148 @@
+function renderModules(content) {
+    const moduleContainer = document.getElementById("modules-container");
+    moduleContainer.innerHTML = "";
+
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("module-card");
+    content.forEach((card) => {
+
+        const wordHeader = document.createElement("li");
+        wordHeader.textContent = `${card.word} - ${card.translation}`;
+        cardDiv.appendChild(wordHeader);
+
+        moduleContainer.appendChild(cardDiv);
+    });
+}
+
+
+function showCards() {
+    const moduleName = window.location.search.split('=')[1];
+    fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
+        queryType: 'get-cards-to-repeat',
+        moduleName: moduleName
+    }))
+        .then(response => response.json())
+        .then(data => {
+            // Сохраняем исходный порядок карточек в переменную originalOrder
+            let originalOrder = data.map((_, index) => index);
+            renderCards(data, originalOrder);
+        })
+        .catch(error => console.error(error));
+    window.location.href = `cards.html?moduleName=${encodeURIComponent(window.location.search.split('=')[1])}`;
+}
+
+function showModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'block';
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = 'none';
+}
+
+function addCard(event) {
+    event.preventDefault();
+
+    const moduleName = window.location.search.split('=')[1];
+    const wordInput = document.getElementById('word-input');
+    const translationInput = document.getElementById('translation-input');
+
+    const cardData = {
+        moduleName: moduleName,
+        word: wordInput.value,
+        translation: translationInput.value
+    };
+
+    wordInput.value = '';
+    translationInput.value = '';
+
+    // Send cardData to localhost:8070 using fetch
+    fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
+        'queryType': 'add-Card'
+    }), {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(cardData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response from the server
+            console.log(data);
+            closeModal('add-card-form');
+            loadCards();
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error('Error:', error);
+        });
+}
+
+function shareModule(event) {
+    event.preventDefault();
+
+    const usernameInput = document.getElementById('username-input');
+    const passwordInput = document.getElementById('password-input');
+
+    const shareData = {
+        username: usernameInput.value,
+        password: passwordInput.value,
+        moduleName: window.location.search.split("=")[1]
+    };
+
+    usernameInput.value = '';
+    passwordInput.value = '';
+
+// Send shareData to localhost:8070 using fetch
+    fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
+        'queryType': 'share-module'
+    }), {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(shareData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response from the server
+            console.log(data);
+            closeModal('share-module-form');
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error('Error:', error);
+        });
+}
+
+function goBack() {
+    window.history.back();
+}
+
+// Fetch moduleData from localhost:8070 using fetch
+
+function loadCards() {
+    const moduleName = window.location.search.split("=")[1];
+    fetch(
+        "http://localhost:8070/EasyLearning?" +
+        new URLSearchParams({
+            queryType: "get-module-data",
+            moduleName: moduleName,
+        })
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            // Handle the response from the server
+            console.log(data);
+            document.getElementById("module-title").textContent = data.name;
+            renderModules(data.content);
+        })
+        .catch((error) => {
+            // Handle any errors
+            console.error("Error:", error);
+        });
+}
+
+loadCards();
---
+++ front/java script/settings.js
@@ -0,0 +1,133 @@
+const confirmPassword = document.getElementById('confirm-password');
+const passwordMismatchMessage = document.querySelector('.password-mismatch-message');
+
+function checkPasswordMatch() {
+    const currentPassword = document.getElementById('new-password').value;
+    if (confirmPassword.value !== '' && confirmPassword.value !== currentPassword) {
+        confirmPassword.style.borderColor = 'red';
+        passwordMismatchMessage.style.display = 'block';
+    } else {
+        confirmPassword.style.borderColor = '';
+        passwordMismatchMessage.style.display = 'none';
+    }
+}
+
+function getData() {
+    fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
+        'queryType': 'get-data-for-settings'
+    }))
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('current-login').textContent = data.login;
+            document.getElementById('current-security-word').textContent = data.securityWord;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+function changeLogin() {
+    const newLogin = document.getElementById('new-login').value;
+
+    if (newLogin) {
+        const data = {login: newLogin};
+
+        fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
+            'queryType': 'change-login'
+        }), {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(result => {
+                if (result.success) {
+                    document.getElementById('current-login').textContent = newLogin;
+                    alert('Логин успешно изменен');
+                    document.getElementById('settings-form').reset();
+                } else {
+                    alert('Этот логин уже занят');
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+}
+
+function changePassword() {
+    const currentPassword = document.getElementById('current-password').value;
+    const newPassword = document.getElementById('new-password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+
+    if (newPassword === confirmPassword) {
+        const data = {
+            currentPassword: currentPassword,
+            newPassword: newPassword
+        };
+
+        fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
+            'queryType': 'change-password'
+        }), {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(result => {
+                if (result.success) {
+                    alert('Пароль успешно изменен');
+                    document.getElementById('settings-form-3').reset();
+                } else {
+                    alert('Неверный пароль');
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    } else {
+        alert('Пароли не совпадают');
+        document.getElementById('confirm-password').style.borderColor = 'red';
+    }
+}
+
+function changeSecurityWord() {
+    const newSecurityWord = document.getElementById('new-security-word').value;
+
+    if (newSecurityWord) {
+        const data = {securityWord: newSecurityWord};
+
+        fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
+            'queryType': 'change-security-word'
+        }), {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(result => {
+                if (result.success) {
+                    document.getElementById('current-security-word').textContent = newSecurityWord;
+                    alert('Кодовое слово успешно изменено');
+                    document.getElementById('settings-form-2').reset();
+                } else {
+                    alert('Попробуйте другое');
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+}
+
+function goBack() {
+    window.history.back();
+}
+
+getData();
