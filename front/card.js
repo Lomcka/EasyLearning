62b7d8dbcea2fdc618083ffa8078b052baa7c0e9
Iren137 <irishavoronkina@gmail.com>
2023-05-27 10:23:36 +0300

    fix cards

--- front/card.js
+++ front/card.js
@@ -2,9 +2,11 @@ const urlParams = new URLSearchParams(window.location.search);
 const jsonData = urlParams.get('data');
 
 const data = JSON.parse(jsonData);
+let okCards = data.ok;
+let repeatCards = data.repeat;
 
-const okPower = data.ok.length;
-const repeatPower = data.repeat.length;
+const okPower = okCards.length;
+const repeatPower = repeatCards.length;
 const totalPower = okPower + repeatPower;
 
 const chartData = [
@@ -20,18 +22,33 @@ Plotly.newPlot('chart', chartData);
 const continueBtn = document.getElementById('continue-btn');
 const resetBtn = document.getElementById('reset-btn');
 
-continueBtn.addEventListener('click', () => {
-    const data = {}; // здесь должен быть ваш объект данных
 
+continueBtn.addEventListener('click', () => {
     fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
-        'queryType': 'return-cards-to-repeat'
+        'queryType': 'resend-ok-repeat'
     }), {
         method: 'POST',
-        body: JSON.stringify(data)
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'ok': okCards,
+            'repeat': repeatCards
+        })
     })
-        .then(() => {
-            window.location.href = '/card.html?data=' + JSON.stringify(data);
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response from the server
+            console.log(data);
+            closeModal('add-card-form');
+            loadCards();
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error('Error:', error);
         });
+    window.location.href = `cards.html`;
+
 });
 
 
@@ -42,12 +59,27 @@ resetBtn.addEventListener('click', () => {
     };
 
     fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
-        'queryType': 'return-cards-to-repeat'
+        'queryType': 'resend-ok-repeat'
     }), {
         method: 'POST',
-        body: JSON.stringify(newData)
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'ok': newData,
+            'repeat': []
+        })
     })
-        .then(() => {
-            window.location.href = '/card.html?data=' + JSON.stringify(newData);
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response from the server
+            console.log(data);
+            closeModal('add-card-form');
+            loadCards();
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error('Error:', error);
         });
+    window.location.href = `cards.html`;
 });
--- front/cards.js
+++ front/cards.js
@@ -1,16 +1,16 @@
 const cardContainer = document.querySelector('.card-container');
 const modal = document.querySelector('.modal');
-const modalTitle = document.querySelector('.modal-header .title');
-const okButton = document.querySelector('.modal-footer .ok-button');
-const repeatButton = document.querySelector('.modal-footer .repeat-button');
-const closeButton = document.querySelector('.modal-header .close-button');
 let currentIndex = 0;
 let cards = [];
 let randomOrder = true;
 let showWordFirst = true;
 let okCards = [];
 let repeatCards = [];
+let cur_translation='';
+let cur_word='';
+let order = [2, 0, 1];
 
+const currentCard = document.createElement('div');
 function showModal(modalId) {
     const modal = document.getElementById(modalId);
     modal.style.display = 'block';
@@ -20,20 +20,23 @@ function closeModal(modalId) {
     const modal = document.getElementById(modalId);
     modal.style.display = 'none';
 }
-function showCard(index) {
-    const card = cards[index];
-    const currentCard = document.createElement('div');
-    currentCard.classList.add('card');
-    currentCard.innerHTML = `
-    <div class="word">${showWordFirst ? card.word : card.translation}</div>
-    <div class="translation">${showWordFirst ? card.translation : card.word}</div>
-  `;
-    cardContainer.innerHTML = '';
-    cardContainer.appendChild(currentCard);
-}
+
+// function showCard(index) {
+//     const card = cards[index];
+//     currentCard.classList.add('card');
+//     currentCard.innerHTML = `
+     // <div class="word">${showWordFirst ? card.word : card.translation}</div>
+     // <div class="translation">${showWordFirst ? card.translation : card.word}</div>
+  // `;
+  //   cardContainer.innerHTML = '';
+  //   cardContainer.appendChild(currentCard);
+// }
 
 function toggleTranslation() {
     const currentCard = cardContainer.querySelector('.card');
+    // console.log("34: "+currentCard.textContent);
+    currentCard.textContent='';
+    currentCard.textContent+=cur_translation;
     currentCard.classList.toggle('show-translation');
 }
 
@@ -44,20 +47,59 @@ function toggleRandomOrder() {
 function toggleShowWordFirst() {
     showWordFirst = !showWordFirst;
 }
+function waitForKeyPress() {
+    return new Promise((resolve) => {
+        document.addEventListener('keydown', (event) => {
+            if (event.code === 'ArrowLeft' || event.code === 'ArrowRight') {
+                resolve(event);
+            }
+        }, { once: true });
+    });
+}
+
+function Relocate(){
+    fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
+        'queryType': 'resend-ok-repeat'
+    }), {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'ok': okCards,
+            'repeat': repeatCards
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response from the server
+            console.log(data);
+            closeModal('add-card-form');
+            loadCards();
+        })
+        .catch(error => {
+            // Handle any errors
+            console.error('Error:', error);
+        });
+    window.location.href = `card.html`;
+}
 
 function handleRightAnswer() {
     const currentCard = cardContainer.querySelector('.card');
     currentCard.classList.add('right-answer');
     setTimeout(() => {
         currentIndex++;
+        // console.log(currentIndex);
+        // console.log(cards.length);
         if (currentIndex >= cards.length) {
-            window.location.href = `card.html?ok=${JSON.stringify(okCards)}&repeat=${JSON.stringify(repeatCards)}`;
+            Relocate();
         } else {
             currentCard.classList.remove('right-answer');
-            showCard(currentIndex);
+            // showCard(order[currentIndex]);
         }
+        currentCard.remove(); // Remove the card from the DOM after the animation
     }, 500);
-    okCards.push(cards[currentIndex]);
+    okCards.push(cards[order[currentIndex]]);
 }
 
 function handleWrongAnswer() {
@@ -66,37 +108,33 @@ function handleWrongAnswer() {
     setTimeout(() => {
         currentIndex++;
         if (currentIndex >= cards.length) {
-            window.location.href = `card.html?ok=${JSON.stringify(okCards)}&repeat=${JSON.stringify(repeatCards)}`;
+            Relocate();
         } else {
             currentCard.classList.remove('wrong-answer');
-            showCard(currentIndex);
+            // showCard(order[currentIndex]);
         }
+        currentCard.remove(); // Remove the card from the DOM after the animation
     }, 500);
-    repeatCards.push(cards[currentIndex]);
+    repeatCards.push(cards[order[currentIndex]]);
 }
+
 
 function openModal() {
     modal.style.display = 'flex';
 }
 
-function initModal() {
-    modalTitle.textContent = 'Settings';
-    repeatButton.textContent = 'Заново';
-    closeButton.innerHTML = '&times;';
-    closeButton.addEventListener('click', closeModal);
-    okButton.addEventListener('click', () => {
-        toggleRandomOrder();
-        closeModal();
-    });
-    repeatButton.addEventListener('click', () => {
-        toggleRandomOrder();
-        closeModal();
-    });
+function Repeat(event) {
+    toggleRandomOrder();
+    closeModal('settings');
 }
 
 document.addEventListener('keydown', event => {
+    // console.log("keydown");
     if (event.code === 'Space') {
         toggleTranslation();
+        let temp = cur_translation;
+        cur_translation = cur_word;
+        cur_word = temp;
     } else if (event.code === 'ArrowRight') {
         handleRightAnswer();
     } else if (event.code === 'ArrowLeft') {
@@ -105,7 +143,7 @@ document.addEventListener('keydown', event => {
 });
 
 function toggleOrder() {
-    var orderText = document.getElementById("order-text");
+    const orderText = document.getElementById("order-text");
     if (orderText.innerHTML === "Прямой порядок") {
         orderText.innerHTML = "Перемешанные";
         // Добавляем функционал для опции "Перемешанные"
@@ -119,50 +157,81 @@ function toggleOrder() {
 
 // Функция для загрузки карточек из JSON-файла
 function loadCards() {
-    fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
-        'queryType': 'get-cards-to-repeat'
-    })
+    // console.log("I\'m loading cards");
+    const moduleName = window.location.search.split('=')[1];
+    /*fetch('http://localhost:8070/EasyLearning?' + new URLSearchParams({
+        queryType: 'get-cards-to-repeat',
+        moduleName: moduleName
+    }))
         .then(response => response.json())
         .then(data => {
             // Сохраняем исходный порядок карточек в переменную originalOrder
-            originalOrder = data.map((_, index) => index);
-            renderCards(data, originalOrder);
+            let originalOrder = data.map((_, index) => index);
+    // renderCards(data, originalOrder);
         })
-        .catch(error => console.error(error)));
+        .catch(error => console.error(error));*/
+    renderCards();
+    // showCard(0);
 }
 
 // Функция для отображения карточек на странице
-function renderCards(cards, order) {
+// function renderCards(cards, order) {
+async function renderCards() {
+    // console.log("I\'m rendering cards");
+    cards = [
+        {
+            "word": "a",
+            "translation": "b",
+            "status": "ok"
+        },
+        {
+            "word": "fgjk",
+            "translation": "chvmb",
+            "status": "ok"
+        },
+        {
+            "word": "lol",
+            "translation": "kek",
+            "status": "ok"
+        }
+    ]
+    // console.log(order);
+    // console.log(cards);
     cardContainer.innerHTML = '';
-    order.forEach(i => {
+    for (const i of order) {
+        console.log(i);
         const card = document.createElement('div');
-        const front = document.createElement('div');
-        const back = document.createElement('div');
+        const word = document.createElement('div');
+        const translation = document.createElement('div');
 
         card.classList.add('card');
-        front.classList.add('front');
-        back.classList.add('back');
-        if (cards[i].status === 'ok') {
-            card.classList.add('ok');
-        } else if (cards[i].status === 'repeat') {
-            card.classList.add('repeat');
-        }
+        word.classList.add('front');
+        translation.classList.add('back');
+        // if (cards[i].status === 'ok') {
+            // card.classList.add('ok');
+        // } else if (cards[i].status === 'repeat') {
+        //     card.classList.add('repeat');
+        // }
 
-        front.textContent = cards[i].front;
-        back.textContent = cards[i].back;
+        word.textContent = cards[i].word;
+        translation.textContent = cards[i].translation;
+        console.log(word.textContent);
+        console.log(translation.textContent);
+        cur_translation = translation.textContent;
+        cur_word = word.textContent;
 
-        card.appendChild(front);
-        card.appendChild(back);
+        card.appendChild(word);
+        // card.appendChild(back);
 
         // Добавляем обработчик клика на карточку
-        card.addEventListener('click', () => {
-            back.classList.toggle('show');
+        card.addEventListener('click', (card, translation) => {
+            translation.classList.toggle('show');
         });
 
         cardContainer.appendChild(card);
-    });
+        await waitForKeyPress();
+    }
 }
 
 
 loadCards();
-initModal();
